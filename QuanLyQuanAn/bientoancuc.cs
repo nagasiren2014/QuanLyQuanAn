68e513606a18e1f43883dd81e4d77801d970de87
nagasiren2014 <nagasiren2014@gmail.com>
2017-11-30 22:14:45 +0700

    zz

--- QuanLyQuanAn/bientoancuc.cs
+++ QuanLyQuanAn/bientoancuc.cs
@@ -14,11 +14,10 @@ namespace QuanLyQuanAn
         public static string  MaNV, TenNhanVien, MaCN,MK;
         public static DataTable BangChiNhanh;
         public static DataTable BangMonAn;
-        public static DataTable BangLichSuKhachHang;
+       
         public static DataTable mn;
         public static DataTable dsTaiKhoan;
         public static int viTriTK;
-<<<<<<< HEAD
         public static int xacNhan = 0;
         public static string connectionString = "Provider=SQLNCLI11;Data Source=VAIO;Integrated Security=SSPI;Initial Catalog=QuanLyQuanAn_Data";
 
@@ -28,9 +27,7 @@ namespace QuanLyQuanAn
 
 
 }
-=======
-        public static string connectionString = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QuanLyQuanAn_Data;Data Source=DESKTOP-QR067OS";
-    }
->>>>>>> 7468b774a4044bcfd44dbd61f19f51e2326b84c7
 
-}
+    }
+
+
