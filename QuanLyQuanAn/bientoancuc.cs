b87ced35a780f63aa1688809adda7e639cb178c2
nguyenluongmyha <nguyenluongmyha@gmail.com>
2017-12-08 23:39:33 +0700

    .up

--- QuanLyQuanAn/bientoancuc.cs
+++ QuanLyQuanAn/bientoancuc.cs
@@ -23,8 +23,8 @@ namespace QuanLyQuanAn
 
         public static DataTable dsKhachHang;
         public static string SDT_KhachHang, Ten_KhachHang, DiaChi_KhachHang, TinhThanh_KhachHang;
-        //public static string connectionString = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QuanLyQuanAn_Data;Data Source=DESKTOP-QR067OS";
-        public static string connectionString = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QuanLyQuanAn_Data;Data Source=VAIO";
+        public static string connectionString = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QuanLyQuanAn_Data;Data Source=DESKTOP-QR067OS";
+        //public static string connectionString = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=QuanLyQuanAn_Data;Data Source=VAIO";
 
 
        public static  List<Items> mon = new List<Items>();
